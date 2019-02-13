e3b7f2fb89075c2ab9ea7caf3eebd5ca588ef75c
hyun0k <hyunyoung9310@gmail.com>
2019-02-13 19:32:48 +0900

    190213 solved

--- src/graph/_11724.java
+++ src/graph/_11724.java
@@ -7,42 +7,37 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 /**
  * @Project : codePlus
  * @Package : graph
- * @FileName : _1260.java
+ * @FileName : _11724.java
  * @Author : hyunyoungkim
- * @Date : Jan 24, 2019
+ * @Date : Feb 13, 2019
  */
-public class _1260 { // DFS와 BFS : http://www.acmicpc.net/1260
+public class _11724 { // 연결요소의 개수 : http://www.acmicpc.net/problem/11724
 
 	/**
 	 * @param args
 	 */
-	static int n, m, v;
-	static int[][] adjMat;
+	static int n, m;
 	static boolean[] visited;
-	static StringBuilder sb;
+	static int[][] adjMat;
 
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-		sb = new StringBuilder();
 
 		st = new StringTokenizer(br.readLine());
 		n = Integer.parseInt(st.nextToken());
 		m = Integer.parseInt(st.nextToken());
-		v = Integer.parseInt(st.nextToken());
+
+		adjMat = new int[n + 1][n + 1];
 
 		visited = new boolean[n + 1];
 		Arrays.fill(visited, false);
-
-		adjMat = new int[n + 1][n + 1];
 
 		for (int i = 0; i < m; i++) {
 			st = new StringTokenizer(br.readLine());
@@ -51,40 +46,24 @@ public class _1260 { // DFS와 BFS : http://www.acmicpc.net/1260
 			adjMat[v1][v2] = adjMat[v2][v1] = 1;
 		}
 
-		dfs(v);
-		sb.append(System.lineSeparator());
-
-		Arrays.fill(visited, false);
-		bfs(v);
-
-		System.out.println(sb);
-
+		int cnt = 0;
+		for (int i = 1; i <= n; i++) {
+			if (!visited[i]) {
+				dfs(i);
+			} else {
+				continue;
+			}
+			cnt++;
+		}
+		System.out.println(cnt);
 	}
 
 	private static void dfs(int start) {
 		visited[start] = true;
-		sb.append(start + " ");
 		for (int i = 1; i <= n; i++) {
 			if (adjMat[start][i] == 1 && !visited[i]) {
 				dfs(i);
 			}
 		}
 	}
-
-	private static void bfs(int start) {
-		Queue<Integer> queue = new LinkedList<>();
-		queue.add(start);
-		visited[start] = true;
-		while (!queue.isEmpty()) {
-			start = queue.poll();
-			sb.append(start + " ");
-			for (int i = 1; i <= n; i++) {
-				if (adjMat[start][i] == 1 && !visited[i]) {
-					visited[i] = true;
-					queue.add(i);
-				}
-			}
-		}
-	}
-
 }
